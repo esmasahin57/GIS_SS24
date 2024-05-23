185322f62208b969a2ab7259c1508589421d0aec
esmasahin57 <esmasahin1502@gmail.com>
2024-05-23 15:49:55 +0200

    3. abgabe mit js

--- GIS_Project/script.js
+++
@@ -1,9 +0,0 @@
-let button = document.getElementById("add");
-button.addEventListener("click", addElement);
-
-function addElement(event){
-    let textfeld = document.getElementById("Text");
-    console.log(textfeld.value)
-    let forumDiv = document.getElementById("forum");
-    let p = document
-}
